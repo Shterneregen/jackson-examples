8a2b12c7d9adb6778cd45fefb326597828184df0
shterneregen <shterneregen@gmail.com>
2020-04-26 02:01:07 +0300

    kebab naming strategy

--- readme.md
+++ readme.md
@@ -1,4 +1,10 @@
 # Jackson Examples with Spring Boot
 
+_PropertyNamingStrategy_
+* Camel case: camelCase
+* Snake case: snake_case
+* Kebab case: kebab-case
+
+---
 Original: 
 [Jackson Examples with Spring Boot](https://github.com/springframeworkguru/mssc-jackson-examples)
---
+++ src/test/java/random/jacksonexamples/model/BeerDtoKebabTest.java
@@ -0,0 +1,25 @@
+package random.jacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("kebab")
+@JsonTest
+public class BeerDtoKebabTest extends BaseTest {
+
+    @Test
+    void testSerialize() throws JsonProcessingException {
+        BeerDto dto = getDto();
+        String json = getObjectMapper().writeValueAsString(dto);
+        System.out.println(json);
+    }
+
+    @Test
+    void testDeserialize() throws JsonProcessingException {
+        String json = "{\"id\":\"7f812d70-f687-4b5c-8a3f-2c90678e18ca\",\"beer-name\":\"BeerName\",\"beer-style\":\"Ale\",\"upc\":123123123,\"price\":12.99,\"created-date\":\"2020-04-26T01:46:07.8465644+03:00\",\"last-updated-date\":\"2020-04-26T01:46:07.8465644+03:00\"}";
+        BeerDto dto = getObjectMapper().readValue(json, BeerDto.class);
+        System.out.println(dto);
+    }
+}
