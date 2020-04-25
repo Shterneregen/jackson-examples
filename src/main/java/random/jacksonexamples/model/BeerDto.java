a1e4370936b178535b07177269175af38b65f64a
shterneregen <shterneregen@gmail.com>
2020-04-26 02:37:50 +0300

    @JsonFormat

--- src/main/java/random/jacksonexamples/model/BeerDto.java
+++ src/main/java/random/jacksonexamples/model/BeerDto.java
@@ -1,5 +1,6 @@
 package random.jacksonexamples.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -32,7 +33,9 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
 }
